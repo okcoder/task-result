79c30e225392d34d7aa57b618817dc7b2b4dbbb9
okcoder <okcoder@live.com>
2020-09-06 10:21:33 +0900

    add ElementUI

--- task-result-ui/src/main.js
+++ task-result-ui/src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.config.productionTip = false
+Vue.use(ElementUI, { locale })
+
 Vue.config.productionTip = false
 
 new Vue({
